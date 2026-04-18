26d904be125d0e1931e571b138abbebc7da8a1ad
Th0rN13 <th0r@inbox.ru>
2026-04-18 21:17:39 +0500

    feat: initial release

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from "@sveltejs/adapter-static";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter(),
+		alias: { $cmp: "src/components" },
+		paths: {
+			base: "/widgets",
+		},
+	},
+	compilerOptions: { runes: true },
+};
+
+export default config;
